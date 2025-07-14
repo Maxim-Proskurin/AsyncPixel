29c98da3e0ad38e869cc4e03ba5f6d3234efa96f
Maxim Proskurin <Mx.prskn@yandex.ru>
2025-07-14 12:15:10 +0300

    добавил модели

---
+++ app/models/task.py
@@ -0,0 +1,45 @@
+import uuid
+from sqlalchemy import Column, String, DateTime, ForeignKey, func
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class Task(Base):
+    
+    __tablename__ = "tasks"
+    
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False
+    )
+    user_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("users.id"),
+        nullable=False,
+        index=True
+    )
+    image_path = Column(
+        String(255),
+        nullable=False
+    )
+    status = Column(
+        String(50),
+        nullable=False,
+        default="pending"
+    )
+    result_path = Column(
+        String(255),
+        nullable=True
+    )
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+    
+    user = relationship("User", back_populates="tasks")
---
+++ app/models/user.py
@@ -0,0 +1,46 @@
+import uuid
+from sqlalchemy import Column, String, DateTime, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class User(Base):
+    
+    __tablename__ = "users"
+    
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False
+    )
+    username = Column(
+        String(50),
+        unique=True,
+        nullable=False,
+        index=True
+    )
+    full_name = Column(
+        String(100),
+        nullable=False
+    )
+    email = Column(
+        String(255),
+        unique=True,
+        nullable=False,
+        index=True
+    )
+    hasged_password = Column(
+        String(255),
+        nullable=False
+    )
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+    
+    tasks = relationship("Task", back_populates="user", cascade="all, delete-orphan")
+    
