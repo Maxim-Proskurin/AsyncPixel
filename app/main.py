87240247fd8aa0a064189e789e684bd99bfa5f95
Maxim Proskurin <Mx.prskn@yandex.ru>
2025-07-15 00:48:40 +0300

    init и минимальный main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from app.api import task
+
+import app.models
+from app.api import auth, user
 
 app = FastAPI(title="AsyncPixel")
 
+
+app.include_router(auth.router)
+app.include_router(user.router)
+app.include_router(task.router)
+
 @app.get("/")
 async def root():
-    return {"message": "Да прибудет AsyncPixel API!"}
+    return {"message": "AsyncPixel API is running!"}
